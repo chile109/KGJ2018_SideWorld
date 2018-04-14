64c84f17c4d86a9478ff3720526108bf0b47c047
H0WT0G0 <a451201@gmail.com>
2018-04-14 18:19:04 +0800

    地圖物件上傳

---
+++ SideProject/Assets/Script/MapInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//單塊地圖的資訊
+
+public class MapInfo : MonoBehaviour {
+
+    //地圖使用的Sprite
+    public SpriteRenderer sp;
+
+    //地圖上的掉落物
+    public SpriteRenderer item;
+
+    //所屬座標
+    public Vector2 pos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ SideProject/Assets/Script/MapManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//地圖控制程式
+
+public class MapManager : MonoBehaviour {
+
+    //單例模式
+    private static MapManager instance = null;
+    public static MapManager Instance {
+        get {
+            // 還沒指定時就先尋找遊戲中有沒有⼀樣的
+            if(instance == null) {
+                instance = FindObjectOfType<MapManager>();
+            }
+            if(instance == null) {
+                print("找不到MapManager");
+            }
+            return instance;
+        }
+    }
+
+    //地圖資訊
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
