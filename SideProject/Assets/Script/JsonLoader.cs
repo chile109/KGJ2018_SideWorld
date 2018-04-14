745a54382d5450940a418e9f874bbb5cd6c786b8
Kevin Chang <kevin@megalive.com.tw>
2018-04-14 20:25:55 +0800

    [Update] Event loader

--- SideProject/Assets/Script/JsonLoader.cs
+++ SideProject/Assets/Script/JsonLoader.cs
@@ -10,8 +10,20 @@ public class ItemData
 
 public class EventData
 {
-    public string sn;
-    public string name;
+    public string Sn;
+    public string Name;
+    public string DescriptionGood;
+    public string DescriptionBad;
+    public int[] got = new int[4];
+    public int[] Lost = new int[4];
+    public int GetMoney;
+    public int LostMoney;
+    public int MinAge;
+    public int MaxAge;
+    public int GoodPic;
+    public int BadPic;
+    public int Repeat;
+    public int End;
 }
 
 public class PlayerData
@@ -36,9 +48,9 @@ public class JsonLoader : MonoBehaviour {
         loadEvent();
         loadRef();
 
-        foreach (var i in ItemPool)
+        foreach (var i in EventPool)
         {
-            Debug.Log(i.name);
+            Debug.Log(i.Name);
         }
     }
 
@@ -71,15 +83,39 @@ public class JsonLoader : MonoBehaviour {
             JsonReader jsonary = new JsonReader(txt.text);
             var jsonData = JsonMapper.ToObject(jsonary);
 
+            Debug.LogFormat("txt is {0}", txt.text);
+
             for (int i = 0; i < jsonData.Count; ++i)
             {
-                EventData _event = new EventData();
+                if (jsonData[i].ToString() != "")
+                {
+                    EventData _event = new EventData();
 
-                _event.sn = jsonData[i].ToString();
-                _event.name = jsonData[i]["Event"].ToString();
+                    _event.Sn = jsonData[i]["Sn"].ToString();
+                    _event.Name = jsonData[i]["Name"].ToString();
+                    _event.DescriptionGood = jsonData[i]["DescriptionGood"].ToString();
+                    _event.DescriptionBad = jsonData[i]["DescriptionGood"].ToString();
 
-                EventPool.Add(_event);
+                    _event.got[1] = (int)jsonData[i]["Get1"];
+                    _event.got[2] = (int)jsonData[i]["Get2"];
+                    _event.got[3] = (int)jsonData[i]["Get3"];
 
+                    _event.Lost[0] = (int)jsonData[i]["Lost0"];
+                    _event.Lost[1] = (int)jsonData[i]["Lost1"];
+                    _event.Lost[2] = (int)jsonData[i]["Lost2"];
+                    _event.Lost[3] = (int)jsonData[i]["Lost3"];
+
+                    _event.GetMoney = (int)jsonData[i]["GetMoney"];
+                    _event.LostMoney = (int)jsonData[i]["LostMoney"];
+                    _event.MinAge = (int)jsonData[i]["MinAge"];
+                    _event.MaxAge = (int)jsonData[i]["MaxAge"];
+                    _event.GoodPic = (int)jsonData[i]["GoodPic"];
+                    _event.BadPic = (int)jsonData[i]["BadPic"];
+                    _event.Repeat = (int)jsonData[i]["Repeat"];
+                    _event.End = (int)jsonData[i]["End"];
+
+                    EventPool.Add(_event);
+                }
             }
         }
     }
