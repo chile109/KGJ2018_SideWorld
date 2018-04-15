feef0da691e1b326de88354c4ff6f74a640a8c71
Kevin Chang <kevin@megalive.com.tw>
2018-04-15 10:19:58 +0800

    [Add] 新增背景 與 接口

---
+++ Assets/Script/PanelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelManager : MonoBehaviour {
+
+    public static PanelManager _inst;
+    public Text _Age;
+    public Text _Job;
+    public Text _Money;
+
+
+	private void Start()
+	{
+        if(_inst = null)
+        {
+            _inst = this;
+        }
+
+	}
+}
